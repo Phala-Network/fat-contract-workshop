c2a39f1b468f7db9a0b628d88e2f4dbe96103171
h4x3rotab <h4x3rotab@gmail.com>
2022-02-13 11:29:50 +0000

    http sample base

--- lib.rs
+++ lib.rs
@@ -1,46 +1,27 @@
 #![cfg_attr(not(feature = "std"), no_std)]
+extern crate alloc;
 
-use ink_lang as ink;
+use pink_extension as pink;
 
-#[ink::contract]
-mod flipper {
+#[pink::contract(env=PinkEnvironment)]
+mod fat_sample {
+    use super::pink;
+    use pink::{PinkEnvironment, http_get};
+    use alloc::vec::Vec;
 
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
     #[ink(storage)]
-    pub struct Flipper {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
-    }
+    pub struct FatSample {}
 
-    impl Flipper {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
-        #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
-        }
-
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
+    impl FatSample {
         #[ink(constructor)]
         pub fn default() -> Self {
-            Self::new(Default::default())
+            Self {}
         }
 
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
-        }
-
-        /// Simply returns the current value of our `bool`.
-        #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
+        pub fn query_example(&self) -> (u16, Vec<u8>) {
+            let resposne = http_get!("https://example.com");
+            (resposne.status_code, resposne.body)
         }
     }
 
@@ -55,20 +36,9 @@ mod flipper {
         /// Imports `ink_lang` so we can use `#[ink::test]`.
         use ink_lang as ink;
 
-        /// We test if the default constructor does its job.
-        #[ink::test]
-        fn default_works() {
-            let flipper = Flipper::default();
-            assert_eq!(flipper.get(), false);
-        }
-
         /// We test a simple use case of our contract.
         #[ink::test]
         fn it_works() {
-            let mut flipper = Flipper::new(false);
-            assert_eq!(flipper.get(), false);
-            flipper.flip();
-            assert_eq!(flipper.get(), true);
         }
     }
 }
